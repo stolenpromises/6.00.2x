4ff60a01d4f0ef6ed329de55ddb8cb4e7bc08cf0
stolenpromises <70161822+stolenpromises@users.noreply.github.com>
2020-12-31 09:22:28 -0600

    Initial commit

---
+++ unit 1/lecture 1/optimization and the knapsack problem/docstring and def vs lambda function.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Practice code written while reviewing pep257
+docstring convention and def vs lambda functions
+
+Created on Wed Dec 30 04:30:57 2020
+
+@author: nathan.m
+"""
+
+def is_even(i):
+    """Evaluate if an integer is even.
+    
+    Keyword arguments:    
+    i -- a positive integer
+    
+    Return defintions:
+        
+    True -- if i is even
+    False -- otherwise
+    """
+    if i%2 == 0:
+        return True
+    else:
+        return False
