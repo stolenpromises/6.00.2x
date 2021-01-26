c9563edea395c3639f411c7a50028c9025fa5610
stolenpromises <70161822+stolenpromises@users.noreply.github.com>
2021-01-26 13:26:24 -0600

    Complete exercise 2
    Remove various draft ways to complete this exercise via the itertools module.
    
    Complete my chosen implementation, which is much longer than the exercise 1 solution... but I feel the exercise 1 solution is prohibitively confusion.

--- unit 1/lecture 2 - decision trees and dynamic programming/exercise 2.py
+++ unit 1/lecture 2 - decision trees and dynamic programming/exercise 2.py
@@ -9,60 +9,29 @@ Created on Fri Jan 22 18:13:09 2021
 
 import itertools
 
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = iterable
-    return itertools.chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
-
-def from_iterable(iterables):
-    # chain.from_iterable(['ABC', 'DEF']) --> A B C D E F
-    for it in iterables:
-        for element in it:
-            yield element
-
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
-
-return chain.from_iterable
-
-set = ['a','b','c']
-
-
-
-def rangeiterate(list):
-    '''
-    Iteratively ranges over the length of a list, returning said length
-    '''
-    for l in range(len(list)):
-        return l
-
-# build tuple combo 1
-def firstlist(list):
+def combogenerator(inputlist):
     """
-    Iteratively generates all tuple combinations of a list
-    """
-    for l in range(len(list)): # range over the length of the list
-        lengthtuple = itertools.combinations(list, l) # create a tuple of that length
-        for item in lengthtuple: # loop over the objects in that tuple
-            yield item
+    Generates all combinations of N items into two bags, whereby each 
+    item is in one or zero bags.
 
-# build tuple combo 2, excluding items found in combo 1
-def secondlist(tuplecombo1, list):
-    prunedlist = []
-    for i in list: # loop over items in the original list
-        if i not in tuplecombo1: # check that the item isn't in combo 1
-            prunedlist.append(i) # append to the pruned list
-    return firstlist(prunedlist)
-        
-# combine tuple combo 1 and 2 into a generated result
-def combothelists():
-            
-        
-    
-    
-    for l in range(len(list)): # range over the length of the list
-        lengthtuple = itertools.combinations(set, l) # create a tuple of that length
-        for item in lengthtuple: # loop over the objects in that tuple
-            yield item
+    Yields a tuple, (bag1, bag2), where each bag is represented as a list 
+    of which item(s) are in each bag.
+    """
+    for l in range(len(inputlist)): # range over the length of the list
+        combo1possibles = itertools.combinations(inputlist, l) # length combo limit
+        # now we are working with list1 possibilities
+        for combination in combo1possibles: # loop over all list1 possibilities
+            list1 = list(combination) # convert list1 combo from tuple to list
+            prunedlist = [] 
+            for i in inputlist: # loop over items in the full list
+                if i not in list1: # check that the item isn't in combo1
+                    prunedlist.append(i) # append to the pruned list
+            # now we are working with list2 possibilties
+            for l in range(len(prunedlist)): # range over the length of the list
+                combo2possibles = itertools.combinations(prunedlist, l) # length combo limit
+                # build combo of list 1 and 2 into a tuple and yield
+                for combination in combo2possibles: # for each list2 possibility
+                    yield (list1, list(combination)) # combine with list1 and yield
+testset = ['a','b','c']          
+x = combogenerator(testset)
+print(x.__next__())
