405a4c9b032f4896de7b40c8ca4df61f2de1ca09
stolenpromises <70161822+stolenpromises@users.noreply.github.com>
2021-01-28 02:02:19 -0600

    Complete exercise 2
    Lot's of practice and review of classes, subclass and superclass.

---
+++ unit 1/lecture 3/exercise 2.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+Code written when answering lecture 3 exercise 2. Note that most code included
+here was not written by me, but rather was supplied with the exercise.
+
+Created on Wed Jan 27 22:25:31 2021
+
+@author: nathan.m
+"""
+
+# exercise supplied code BEGIN
+
+class Node(object):
+    def __init__(self, name):
+        """Assumes name is a string"""
+        self.name = name
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
+
+class Edge(object):
+    def __init__(self, src, dest):
+        """Assumes src and dest are nodes"""
+        self.src = src
+        self.dest = dest
+    def getSource(self):
+        return self.src
+    def getDestination(self):
+        return self.dest
+    def __str__(self):
+        return self.src.getName() + '->' + self.dest.getName()
+               
+class Digraph(object):
+    """edges is a dict mapping each node to a list of
+    its children"""
+    def __init__(self):
+        self.edges = {}
+    def addNode(self, node):
+        if node in self.edges:
+            raise ValueError('Duplicate node')
+        else:
+            self.edges[node] = []
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not (src in self.edges and dest in self.edges):
+            raise ValueError('Node not in graph')
+        self.edges[src].append(dest)
+    def childrenOf(self, node):
+        return self.edges[node]
+    def hasNode(self, node):
+        return node in self.edges
+    def getNode(self, name):
+        for n in self.edges:
+            if n.getName() == name:
+                return n
+        raise NameError(name)
+    def __str__(self):
+        result = ''
+        for src in self.edges:
+            for dest in self.edges[src]:
+                result = result + src.getName() + '->'\
+                         + dest.getName() + '\n'
+        return result[:-1] #omit final newline
+
+class Graph(Digraph):
+    def addEdge(self, edge):
+        Digraph.addEdge(self, edge)
+        rev = Edge(edge.getDestination(), edge.getSource())
+        Digraph.addEdge(self, rev)
+
+    
+def buildCityGraph(graphType):
+    g = graphType()
+    for name in ('Boston', 'Providence', 'New York', 'Chicago',
+                 'Denver', 'Phoenix', 'Los Angeles'): #Create 7 nodes
+        g.addNode(Node(name))
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('Providence')))
+    g.addEdge(Edge(g.getNode('Boston'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('Boston')))
+    g.addEdge(Edge(g.getNode('Providence'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('New York'), g.getNode('Chicago')))
+    g.addEdge(Edge(g.getNode('Chicago'), g.getNode('Denver')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('Phoenix')))
+    g.addEdge(Edge(g.getNode('Denver'), g.getNode('New York')))
+    g.addEdge(Edge(g.getNode('Los Angeles'), g.getNode('Boston')))
+    return g
+
+nodes = []
+nodes.append(Node("ABC")) # nodes[0]
+nodes.append(Node("ACB")) # nodes[1]
+nodes.append(Node("BAC")) # nodes[2]
+nodes.append(Node("BCA")) # nodes[3]
+nodes.append(Node("CAB")) # nodes[4]
+nodes.append(Node("CBA")) # nodes[5]
+
+g = Graph()
+for n in nodes:
+    g.addNode(n)
+    
+# exercise supplied code END
+          
+for i in nodes:# iterate over the node entries  
+    nodestr = str(i)# make a string from the node name
+    # create possible permutations
+    middleleft = nodestr[1] + nodestr[0] + nodestr[2]
+    middleright = nodestr[0] + nodestr[2] + nodestr[1]
+    for checknode in nodes:# iterate over the node entries
+        if str(checknode) == middleleft or str(checknode) == middleright:# check for a valid permutation
+            # initiate the edge via the digraph method
+            #g.self passed in, followed by the subclass
+            Digraph.addEdge(g, Edge(i, checknode))
