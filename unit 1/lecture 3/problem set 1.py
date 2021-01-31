f95707c5357eef3507e5ab0ffd4588c26a2220a8
stolenpromises <70161822+stolenpromises@users.noreply.github.com>
2021-01-31 01:43:12 -0600

    Draft problem set 1 dictionary sorting
    Learned list comprehension followed by dictionary comprehension.

--- unit 1/lecture 3/problem set 1.py
+++ unit 1/lecture 3/problem set 1.py
@@ -13,6 +13,7 @@ Created on Sat Jan 30 20:29:33 2021
 
 @author: nathan.m
 """
+import itertools
 
 
 def greedy_cow_transport(input_dict, cargo_limit):
@@ -35,9 +36,40 @@ def greedy_cow_transport(input_dict, cargo_limit):
     list_of_lists : list
         A list of lists. Each inner list represents a round trip transport
         operation. Payloads are identified via unique_payload parameter passed
-        in at runtime. Each trip orders heavier payloads first but order of
-        trips is arbitrary.
+        in at runtime. Each trip orders heavier payloads first. Arbitrary
+        tiebreak and trip order.
             example:
                 list_of_lists = [[pl1, pl2], [pl3, pl4]]
     """
-d = {}
+
+
+    # convert the input dictionary into a dictionary sorted by weight
+    sorted_dict = input_dict.
+    itertools.combinations(input_dict, r)
+    dict.items
+    sorted()
+    
+# sample dictionary for testing
+sample = {'lpha': 550, 'beta': 1000}
+print(sorted(sample))
+print(sample['beta'])
+for i in sample:
+    print(i)
+print(sample.values())
+sample.items()
+
+sorted_dict = {key:value for (key,value) in sorted(sample.items())}
+
+PyDict_Copy(sample)
+sample.copy
+sorted(sample, key=sample.__getitem__)
+sorted_dict[key, value for (key, value) in sorted(sample.items())]
+[[key, value] for (key, value) in sorted(sample.items())]
+
+dict1 = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+# Double each value in the dictionary
+double_dict1 = {k:v*2 for (k,v) in dict1.items()}
+print(double_dict1)
+
+{'e': 10, 'a': 2, 'c': 6, 'b': 4, 'd': 8}
+
