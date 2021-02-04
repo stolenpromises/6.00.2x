23c2ecbc0be17a0a9549e3448811868aaf082e37
stolenpromises <70161822+stolenpromises@users.noreply.github.com>
2021-02-04 07:42:22 -0600

    Complete ps1 part1
    Infinite loop solved and the previous implementation did work to produce the correct output.
    
    However, the online python grade uses python 3.5... which doesn't support dictionary sorting int he way I implemented it.
    
    Modified my dictionary-based implementation to a list-based implementation to resolved this. Both are present in part 1 of this file.
    
    Misc testing code left in place for now, as I might need it for part 2-3.

--- unit 1/lecture 3/problem set 1.py
+++ unit 1/lecture 3/problem set 1.py
@@ -13,9 +13,61 @@ Created on Sat Jan 30 20:29:33 2021
 
 @author: nathan.m
 """
-import itertools
+
+# part 1 BEGIN
 
 
+# python 3.6.2+
+def greedy_cow_transport_modern(input_dict, cargo_limit):
+    """
+    Optimizes a multi-part transport operation.
+
+    Parameters
+    ----------
+    input_dict : dict
+        dictionary matching payload item names to weights(in tons).
+            example entry/types/max values:
+                {unique_payload: 100}
+                {str: int}
+                {n/a: 0-100}
+    cargo_limit : int
+        weight limit per transport operation
+
+    Returns
+    -------
+    cargo_history : list
+        A list of lists. Each inner list represents a round trip transport
+        operation. Payloads are identified via unique_payload parameter passed
+        in at runtime. Each trip orders heavier payloads first. Arbitrary
+        tiebreak and trip order.
+            example:
+                cargo_history = [[pl1, pl2], [pl3, pl4]]
+    """
+    # helper function to allow for sorting the dictionary by value
+    def valuetarget(x):
+        """Return dictionary item values."""
+        return x[1]
+
+    # convert the input dictionary into a dictionary sorted by weight
+    sorted_dict = {key: value for (key, value) in sorted(input_dict.items(), key=valuetarget, reverse=True)}
+    cargo_history = []  # initiate the list of lists
+    # run a takeoff loop as long as there is remaining payload
+    while sorted_dict != {}:
+        # plane landed empty
+        cargo = []
+        cargo_weight = cargo_limit
+        # load payload biggest to smallest and don't exceed the cargo limit
+        for i in sorted_dict:  # loop over remaining payload
+            if sorted_dict[i] <= cargo_weight:  # cargo room for payload
+                cargo.append(i)  # add the payload to cargo
+                cargo_weight -= sorted_dict[i]  # add the payload weight
+        for i in cargo:  # remove loaded payload
+            del(sorted_dict[str(i)])  # payload accounted for... remove
+        cargo_history.append(cargo)  # record the delivery
+    return cargo_history
+
+
+# python 3.5
 def greedy_cow_transport(input_dict, cargo_limit):
     """
     Optimizes a multi-part transport operation.
@@ -43,45 +95,45 @@ def greedy_cow_transport(input_dict, cargo_limit):
     """
     # helper function to allow for sorting the dictionary by value
     def valuetarget(x):
+        """Return dictionary item values."""
         return x[1]
 
-    # convert the input dictionary into a dictionary sorted by weight
-    sorted_dict = {key: value for (key, value) in sorted(input_dict.items(), key = valuetarget, reverse=True)}
-    print('transport beginning. sorted_dict is ', sorted_dict)
+    # convert the input dictionary into a list of lists sorted by weight
+    lolsorted = []
+    lolsorted = [[k, v] for (k, v) in sorted(input_dict.items(), key=valuetarget, reverse=True)]
     cargo_history = []  # initiate the list of lists
     # run a takeoff loop as long as there is remaining payload
-    # iterations = 0
-    while sorted_dict != {}:
-    # while iterations <3:
-        # iterations += 1
+    while lolsorted != []:
         # plane landed empty
+        purgelist = []
         cargo = []
         cargo_weight = cargo_limit
-        dict_trash_que = []  # prep a trash que for payload that we load
         # load payload biggest to smallest and don't exceed the cargo limit
-        print('load payload loop begins')
-        print()
-        for i in sorted_dict:  # loop over remaining payload
-            print('remaining payload loop beginning on ', i)
-            if sorted_dict[i] <= cargo_weight:  # cargo room for payload
-                print('there is cargo room')
-                cargo.append(i)  # add the payload to cargo
-                print('cargo appended. cargo is now ', cargo)
-                cargo_weight -= sorted_dict[i]  # add the payload weight
-                print('payload weight is added. cargo_weight is now ', cargo_weight)
-        print('sorted dict removal loop beginning')
-        for i in cargo:  # remove loaded payload
-            print('dict_trash_que working on ', i)
-            del(sorted_dict[str(i)])  # payload accounted for... remove
-        print('trash que ending and sorted dict now is ', sorted_dict)
+        for i in range(len(lolsorted)):  # loop over remaining payload
+            if lolsorted[i][1] <= cargo_weight:  # cargo room for payload
+                cargo.append(lolsorted[i][0])  # add the payload to cargo
+                cargo_weight -= lolsorted[i][1]  # add the payload weight
+                purgelist.append(lolsorted[i])  # append for removal
+        for i in purgelist:  # loop over payload which was loaded
+            lolsorted.remove(i)  # payload accounted for... remove
         cargo_history.append(cargo)  # record the delivery
-        print('cargo_history now is ', cargo_history)
+    return cargo_history
 
 # sample dictionary for testing
-sample = {"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}
-samplelimit = 10
-print(greedy_cow_transport(sample, samplelimit))
+# sample = {"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}
+# samplelimit = 10
+# print(greedy_cow_transport(sample, samplelimit))
 
 #temp
-cargo_weight = 10
-test = ('Callie', 2)
+# cargo_weight = 10
+# test = ('Callie', 2)
+# x = (greedy_cow_transport({'Milkshake': 75, 'Clover': 5, 'Patches': 60, 'Muscles': 65, 'Louis': 45, 'Miss Bella': 15, 'Polaris': 20, 'Lotus': 10, 'Horns': 50, 'MooMoo': 85}, 100))
+
+# x = {'Milkshake': 75, 'Clover': 5, 'Patches': 60, 'Muscles': 65, 'Louis': 45, 'Miss Bella': 15, 'Polaris': 20, 'Lotus': 10, 'Horns': 50, 'MooMoo': 85}
+# lolsorted = [[k, v] for (k, v) in sorted(x.items(), key=valuetarget, reverse=True)]
+# def valuetarget(x):
+#     return x[1]
+
+# part 1 END
+# part 2 BEGIN
+
