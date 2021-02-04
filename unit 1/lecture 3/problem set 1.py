7130731888f6235809f42827a58df0023fb60098
stolenpromises <70161822+stolenpromises@users.noreply.github.com>
2021-02-03 19:00:57 -0600

    Complete problems set 1 part 1
    About to remove print statements

--- unit 1/lecture 3/problem set 1.py
+++ unit 1/lecture 3/problem set 1.py
@@ -41,26 +41,47 @@ def greedy_cow_transport(input_dict, cargo_limit):
             example:
                 cargo_history = [[pl1, pl2], [pl3, pl4]]
     """
+    # helper function to allow for sorting the dictionary by value
+    def valuetarget(x):
+        return x[1]
 
     # convert the input dictionary into a dictionary sorted by weight
-    sorted_dict = {key: value for (key, value) in sorted(sample.items())}
+    sorted_dict = {key: value for (key, value) in sorted(input_dict.items(), key = valuetarget, reverse=True)}
+    print('transport beginning. sorted_dict is ', sorted_dict)
     cargo_history = []  # initiate the list of lists
     # run a takeoff loop as long as there is remaining payload
+    # iterations = 0
     while sorted_dict != {}:
+    # while iterations <3:
+        # iterations += 1
         # plane landed empty
         cargo = []
         cargo_weight = cargo_limit
         dict_trash_que = []  # prep a trash que for payload that we load
         # load payload biggest to smallest and don't exceed the cargo limit
+        print('load payload loop begins')
+        print()
         for i in sorted_dict:  # loop over remaining payload
+            print('remaining payload loop beginning on ', i)
             if sorted_dict[i] <= cargo_weight:  # cargo room for payload
+                print('there is cargo room')
                 cargo.append(i)  # add the payload to cargo
-                cargo_weight += sorted_dict[i]  # add the payload weight
-        for i in dict_trash_que:  # remove loaded payload
-            del(sorted_dict[i])  # payload accounted for... remove
+                print('cargo appended. cargo is now ', cargo)
+                cargo_weight -= sorted_dict[i]  # add the payload weight
+                print('payload weight is added. cargo_weight is now ', cargo_weight)
+        print('sorted dict removal loop beginning')
+        for i in cargo:  # remove loaded payload
+            print('dict_trash_que working on ', i)
+            del(sorted_dict[str(i)])  # payload accounted for... remove
+        print('trash que ending and sorted dict now is ', sorted_dict)
         cargo_history.append(cargo)  # record the delivery
+        print('cargo_history now is ', cargo_history)
 
 # sample dictionary for testing
 sample = {"Jesse": 6, "Maybel": 3, "Callie": 2, "Maggie": 5}
 samplelimit = 10
 print(greedy_cow_transport(sample, samplelimit))
+
+#temp
+cargo_weight = 10
+test = ('Callie', 2)
