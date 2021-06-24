1fabba468f3aa5ad82aa374154836162d0287848
stolenpromises <70161822+stolenpromises@users.noreply.github.com>
2021-06-24 07:25:46 -0500

    Complete problems 5-6

--- unit_2/problem_set_2/ps2.py
+++ unit_2/problem_set_2/ps2.py
@@ -254,8 +254,7 @@ class StandardRobot(Robot):
     """
 
     def updatePositionAndClean(self):
-        """
-        Simulate the passage of a single time-step.
+        """Simulate the passage of a single time-step.
 
         Move the robot to a new position and mark the tile it is on as having
         been cleaned.
@@ -297,15 +296,6 @@ def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
     robot_type: class of robot to be instantiated (e.g. StandardRobot or
                 RandomWalkRobot)
     """
-# =============================================================================
-#     num_robots = 2
-#     num_trials = 10
-#     num_robots = 3
-#     width = 20
-#     height = 20
-#     speed = 10
-#     min_coverage = 1
-# =============================================================================
     trial_outcomes = {}  # dict of trial:time_steps
     # loop over each trial
     for trial in range(num_trials):
@@ -327,30 +317,48 @@ def runSimulation(num_robots, speed, width, height, min_coverage, num_trials,
     for trial in trial_outcomes:
         time_step_sum += trial_outcomes[trial]  # append a trial outcome
     return time_step_sum / num_trials  # return the average
+
 # =============================================================================
 # Uncomment this line to see how much your simulation takes on average
-#print(runSimulation(1, 1.0, 10, 10, 0.75, 30, StandardRobot))
+# print(runSimulation(1, 1.0, 10, 10, 0.75, 30, StandardRobot))
+# =============================================================================
+
 
 # === Problem 5
 class RandomWalkRobot(Robot):
+    """A RandomWalkRobot is a robot with the "random walk" movement strategy.
+
+    It chooses a new direction at random at the end of each time-step.
     """
-    A RandomWalkRobot is a robot with the "random walk" movement strategy: it
-    chooses a new direction at random at the end of each time-step.
-    """
+
     def updatePositionAndClean(self):
-        """
-        Simulate the passage of a single time-step.
+        """Simulate the passage of a single time-step.
 
         Move the robot to a new position and mark the tile it is on as having
         been cleaned.
         """
-        raise NotImplementedError
+        # establish a target destination
+        destination = self.position.getNewPosition(self.angle, self.speed)
+        # check the destination against the room
+        if self.room.isPositionInRoom(destination) is True:
+            self.setRobotPosition(destination)  # move to the destination
+            # position variables for readability and float round for grader
+            x = math.floor(destination.getX())
+            y = math.floor(destination.getY())
+            if self.room.isTileCleaned(x, y) is False:  # check for dirt
+                self.room.cleanTileAtPosition(destination)  # clean the tile
+            self.angle = random.uniform(0, 360)  # set a new random angle
+        else:  # the destination falls outside the room
+            self.angle = random.uniform(0, 360)  # set a new random angle
+
+# =============================================================================
+# Uncomment this line to see how much your simulation takes on average
+# print(runSimulation(1, 1.0, 10, 10, 0.75, 30, RandomWalkRobot))
+# =============================================================================
 
 
 def showPlot1(title, x_label, y_label):
-    """
-    What information does the plot produced by this function tell you?
-    """
+    """Mean time steps for a pack of 1-10 standard vs random walk robots."""
     num_robot_range = range(1, 11)
     times1 = []
     times2 = []
@@ -366,10 +374,12 @@ def showPlot1(title, x_label, y_label):
     pylab.ylabel(y_label)
     pylab.show()
 
-    
+
 def showPlot2(title, x_label, y_label):
-    """
-    What information does the plot produced by this function tell you?
+    """2 standard vs random walk robots time steps in rooms of various size.
+
+    X axis: Mean time steps
+    room
     """
     aspect_ratios = []
     times1 = []
@@ -387,25 +397,23 @@ def showPlot2(title, x_label, y_label):
     pylab.xlabel(x_label)
     pylab.ylabel(y_label)
     pylab.show()
-    
 
+
+# =============================================================================
 # === Problem 6
-# NOTE: If you are running the simulation, you will have to close it 
-# before the plot will show up.
-
-#
 # 1) Write a function call to showPlot1 that generates an appropriately-labeled
 #     plot.
 #
-#       (... your call here ...)
-#
-
+# showPlot1('Mean time steps for a pack of 1-10 standard vs random walk robots',
+#           'Robot pack size', 'Mean time steps')
 #
 # 2) Write a function call to showPlot2 that generates an appropriately-labeled
 #     plot.
 #
-#       (... your call here ...)
-#
+showPlot2('Mean time steps for a pack of 2 standard vs random walk robots '
+          'in various sized rooms', 'Room aspect ratio', 'mean time steps')
+# =============================================================================
+
 
 # =============================================================================
 # # simple test case for problem 1
